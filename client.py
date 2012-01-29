128ce05972cf8dd2c1e1d535610f21f6377ac5d1
Zhehao Mao <zhehao.mao@gmail.com>
2012-01-29 00:02:48 -0500

    add client

---
+++ client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import redis
+import json
+import sys
+import os
+import pynotify as notify
+
+if __name__ == '__main__':
+    notify.init("notistream")
+
+    if len(sys.argv) < 2:
+        f = open(os.path.expanduser('~/.notistream.json'))
+    else:
+        f = open(sys.argv[1])
+
+    config = json.load(f)
+
+    r = redis.StrictRedis(host=config['host'], port=config['port'])
+    ps = r.pubsub()
+    ps.subscribe(config['channels'])
+
+    for msg in ps.listen():
+        n = notify.Notification(msg['data'])
+        n.show()
+
