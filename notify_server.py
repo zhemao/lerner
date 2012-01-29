5d058fff5f1cb2138291a7854332e9731723edb4
Zhehao Mao <zhehao.mao@gmail.com>
2012-01-28 20:37:01 -0500

    first commit

---
+++ notify_server.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import redisd
+import pymongo
+
+class Notifier:
+    def __init__(self):
+        self.channels = {}
+        self.db = pymongo.Connection().notistream
+        self.commands = {'subscribe': self.subscribe,
+                         'publish': self.publish}
+
+    def login(self, sock, name, key):
+        user = self.db.users.find({'name': name})
+
+        if user is None:
+            sock.rep_error('No such user')
+        elif user.key != key:
+            sock.rep_error('Wrong key')
+        else:
+            sock.rep_line('OK')
+
+    def subscribe(self, sock, *channames):
+        for i, name in enumerate(channames):
+            chan = self.channels.get(name)
+            if chan is None:
+                self.channels[name] = {sock.sock.fileno(): sock}
+            else:
+                chan[sock.sock.fileno()] = sock
+            sock.rep_multibulk(['subscribe', name, i+1])
+
+    def publish(self, sock, channame, message):
+        chan = self.channels.get(channame)
+        if chan is None:
+            self.channels[channame] = {}
+            sock.rep_integer(0)
+        else:
+            for fileno in chan:
+                chan[fileno].rep_multibulk(['message', channame, message])
+            sock.rep_integer(len(chan))
+
+
+if __name__ == '__main__':
+    notifier = Notifier()
+    server = redisd.RedisServer(('127.0.0.1', 6379), notifier.commands)
+    server.serve_forever()
---
+++ redisd.py
@@ -0,0 +1,80 @@
+from gevent.server import StreamServer
+
+BUFSIZE=1024
+CRLF = '\r\n'
+
+def sock_readlines(socket):
+    remainder = ''
+    while True:
+        data = socket.recv(BUFSIZE)
+        if len(data) == 0:
+            break
+        if CRLF in data:
+            lines = data.split(CRLF)
+            yield remainder + lines[0]
+            for line in lines[1:-1]:
+                yield line
+            if data.endswith(CRLF):
+                yield lines[-1]
+                remainder = ''
+            else:
+                remainder = lines[-1]
+
+
+class RedisSocket:
+    def __init__(self, sock):
+        self.sock = sock
+        return
+
+    def rep_line(self, line):
+        self.sock.send('+' + line + CRLF)
+        return
+
+    def rep_integer(self, num):
+        self.sock.send(':' + str(num) + CRLF)
+
+    def rep_error(self, errmsg):
+        self.sock.send('-' + errmsg + CRLF)
+
+    def rep_bulk(self, data):
+        self.sock.send('$' + str(len(data)) + CRLF + data + CRLF)
+
+    def rep_multibulk(self, lst):
+        self.sock.send('*' + str(len(lst)) + CRLF)
+        for data in lst:
+            if isinstance(data, int):
+                self.sock.send(':' + str(data) + CRLF)
+            else:
+                self.sock.send('$' + str(len(data)) + CRLF)
+                self.sock.send(data + CRLF)
+
+class RedisServer(StreamServer):
+    def __init__(self, addr, commands):
+        StreamServer.__init__(self, addr, self.handle_connection)
+        self.commands = commands
+
+    def handle_connection(self, sock, addr):
+        cmdargs = []
+        ncmds = 0
+        nbytes = 0
+        rdsock = RedisSocket(sock)
+
+        for line in sock_readlines(sock):
+            if len(line) == 0:
+                continue
+            elif line[0] == '*' and ncmds == 0:
+                ncmds = int(line[1:])
+            elif line[0] == '$' and nbytes == 0:
+                nbytes = int(line[1:])
+            else:
+                cmdargs.append(line)
+                nbytes = 0
+                if len(cmdargs) == ncmds:
+                    ncmds = 0
+                    command = self.commands.get(cmdargs[0])
+                    if command is None:
+                        rdsock.rep_error('No such command')
+                    else:
+                        command(rdsock, *cmdargs[1:])
+                    cmdargs = []
+
