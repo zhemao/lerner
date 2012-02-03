a4090b23c5b3b3b1fd8e3b6a9e49e4f994b98e84
Zhehao Mao <zhehao.mao@gmail.com>
2012-02-03 18:01:52 -0500

    rename to lerner

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='Notistream',
+setup(name='Lerner',
       version='0.1',
       description='Streaming desktop notifications from the web',
       author='Zhehao Mao',
       author_email='zhehao.mao@gmail.com',
-      url='https://github.com/zhemao/notistream',
-      scripts=['notistream'],
+      url='https://github.com/zhemao/lerner',
+      scripts=['lerner'],
       dependencies=['redis', 'PyGObject']
       )
